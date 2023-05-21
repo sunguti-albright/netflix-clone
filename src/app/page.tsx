c7446f4b5db795997b5bc094eeedaab98597f321
sunguti-albright <sungutialbright@gmail.com>
2023-05-21 09:34:23 +0300

    folder restructuring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from './header'
+import Header from '../components/header'
 
 export default function Home() {
   return (
-<div className="h-screen w-screen text-red-400 bg-blue-200 p-12">
-<Head>
-  <title>Home - Netflix</title>
-  <link rel="icon" href="/favicon.ico"> </link>
-</Head>
-<Header/>
-<main>
-</main>
-<p>Netflix clone</p>
-</div>
+    <div className="h-screen w-screen text-red-400 bg-blue-200 p-12">
+   
+      <Header />
+      <main>
+      </main>
+      <p>Netflix clone</p>
+    </div>
   )
 }
